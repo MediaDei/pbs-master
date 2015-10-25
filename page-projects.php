5b3ecefcb52bd769f71d2cfd7cfd407f211565cf
phasstw <tylerphass@phasstw.home>
2015-10-25 16:33:12 -0400

    added blur to projects page

--- page-projects.php
+++ page-projects.php
@@ -10,12 +10,30 @@
 	</article>
 </section>
 <section>
+	<script>
+		$(document).ready(function() {
+				var radioButtons = [false, false, false, false, false];
 
-	<input id="project-1st" class="radioButton-project" type="radio" name="project1">
-	<input id="project-2nd" class="radioButton-project" type="radio" name="project2">
-	<input id="project-3rd" class="radioButton-project" type="radio" name="project3">
-	<input id="project-4th" class="radioButton-project" type="radio" name="project4">
-	<input id="project-5th" class="radioButton-project" type="radio" name="project5">
+				$("input[type='radio'].radioButton-project").click(function() {
+					if( radioButtons[$(this).index("input[type='radio']")] == true ) { // if clicking on self
+						$(this).prop('checked', false);								   // unselect self
+						radioButtons[$(this).index("input[type='radio']")] = false;
+					}
+					else {															// if clicking another
+						radioButtons = [false, false, false, false, false];
+						$("input[type='radio'].radioButton-project").prop('checked', false);	// unselect all
+						radioButtons[$(this).index("input[type='radio']")] = true;	// select self
+						$(this).prop('checked', true);
+					}
+				});
+			});
+	</script>
+
+	<input class="radioButton-project" id="project-1st" type="radio" name="project1">
+	<input class="radioButton-project" id="project-2nd" type="radio" name="project2">
+	<input class="radioButton-project" id="project-3rd" type="radio" name="project3">
+	<input class="radioButton-project" id="project-4th" type="radio" name="project4">
+	<input class="radioButton-project" id="project-5th" type="radio" name="project5">
 
 	<h1>
 		Projects
@@ -24,18 +42,18 @@
 		<figure class="img1">
 			<label class="radioLabel-project" for="project-1st">
 			</label>
-			<figcaption>
+			<figcaption class="first">
 				<h2>
 					Anglican Way
 				</h2>
-				<p>
+				<p class="first">
 					Premier online magazine of Anglican faith and tradition.
 				</p>
 			</figcaption>
-			<div class="arrow"></div>
+			<div class="arrow first"></div>
 		</figure>
 	</div>
-	<div class="modal-info">
+	<div class="modal-info m1">
 		<div class="bio">
 			<h2>
 				Anglican Way
@@ -51,18 +69,18 @@
 		<figure class="img2">
 			<label class="radioLabel-project" for="project-2nd">
 			</label>
-			<figcaption>
+			<figcaption class="second">
 				<h2>
 					Anglican Way Print
 				</h2>
-				<p>
+				<p class="second">
 					Print edition on the vital topics in the Church.
 				</p>
 			</figcaption>
-			<div class="arrow"></div>
+			<div class="arrow second"></div>
 		</figure>
 	</div>
-	<div class="modal-info">
+	<div class="modal-info m2">
 		<div class="bio">
 			<h2>
 				Anglican Way Print
@@ -78,18 +96,18 @@
 		<figure class="img3">
 			<label class="radioLabel-project" for="project-3rd">
 			</label>
-			<figcaption>
+			<figcaption class="third">
 				<h2>
 					I-Pray BCP App
 				</h2>
-				<p>
+				<p class="third">
 					Your portable Daily office, going everywhere you go.
 				</p>
 			</figcaption>
-			<div class="arrow"></div>
+			<div class="arrow third"></div>
 		</figure>
 	</div>
-	<div class="modal-info">
+	<div class="modal-info m3">
 		<div class="bio">
 			<h2>
 				I-Pray BCP App
@@ -105,18 +123,18 @@
 		<figure class="img4">
 			<label class="radioLabel-project" for="project-4th">
 			</label>
-			<figcaption>
+			<figcaption class="fourth">
 				<h2>
 					Catechesis Project
 				</h2>
-				<p>
+				<p class="fourth">
 					What is whispered in the classroom, will be uttered on the street.
 				</p>
 			</figcaption>
-			<div class="arrow"></div>
+			<div class="arrow fourth"></div>
 		</figure>
 	</div>
-	<div class="modal-info">
+	<div class="modal-info m4">
 		<div class="bio">
 			<h2>
 				Catechesis Project
@@ -132,18 +150,18 @@
 		<figure class="img5">
 			<label class="radioLabel-project" for="project-5th">
 			</label>
-			<figcaption>
+			<figcaption class="fifth">
 				<h2>
 					New Scriptorium
 				</h2>
-				<p>
+				<p class="fifth">
 					Anglican classics online, for today's generation.
 				</p>
 			</figcaption>
-			<div class="arrow"></div>
+			<div class="arrow fifth"></div>
 		</figure>
 	</div>
-	<div class="modal-info">
+	<div class="modal-info m5">
 		<div class="bio">
 			<h2>
 				New Scriptorium
@@ -154,24 +172,6 @@
 			</p>
 		</div>
 	</div>
-
-	<script>
-		$(document).ready(function() {
-			var radioButtons = [false, false, false, false, false];
-			$("input[type='radio'].radioButton-project").click(function() {
-				if( radioButtons[$(this).index("input[type='radio']")] == true ) { // if clicking on self
-					$(this).prop('checked', false);								   // unselect self
-					radioButtons[$(this).index("input[type='radio']")] = false;
-				}
-				else {															// if clicking another
-					radioButtons = [false, false, false, false, false];
-					$("input[type='radio'].radioButton-project").prop('checked', false);	// unselect all
-					radioButtons[$(this).index("input[type='radio']")] = true;	// select self
-					$(this).prop('checked', true);
-				}
-			});
-		});
-	</script>
 </section>
 
 <?php get_footer(); ?>
