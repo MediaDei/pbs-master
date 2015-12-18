6545f7f47a48119d8f2d06add59f70e026464967
mediadei-james <mediadei-james@users.noreply.github.com>
2015-12-18 11:52:47 -0500

    New Pages: Print Magazine, and Catechesis
    Two new pages have been added:
    -/print-magazine/
    -/catechesis-project/

---
+++ page-catechesis-project.php
@@ -0,0 +1,84 @@
+<?php get_header(); ?>
+
+	
+	<div role="img" class="hero-image resources"></div>
+	<div class="page-title"><h1>Catechesis Project</h1></div>
+</header>
+
+
+<section class="catechesis-project">
+	<h1>Training in the Anglican Faith</h1>
+
+	<h2>The Common Prayer Catechesis Project: Rooted in 
+	the Faith, Bearing Fruit for the Future.</h2>
+	<br>
+
+	<img class="logo" src="<?php bloginfo('template_url'); ?>/images/catechesis-project-logo.jpg"/>
+	<p>The Common Prayer Catechesis Project is a curriculum for discipling, teaching, and training up children, youth, and adults in the Anglican Way of Faith, Worship, and Life.</p>
+
+	<ul class="p">
+		<li><span class="smallcaps">Grounded</span> in the Bible, the Creeds, the classical Anglican Prayer Books and Articles of Religion</li>
+		<li><span class="smallcaps">Age-appropriate</span>—for children, for youth, for adults</li>
+		<li><span class="smallcaps">User-friendly</span>—equipping volunteers for effective teaching</li>
+		<li><span class="smallcaps">Adaptable</span> for small missions and family-size churches, as well as larger, established program churches</li>
+	</ul>
+
+
+	
+		<ul class="p">
+		The curriculum guides learning in these core areas:<li><ul>
+			<li><i>The Prayer Book Catechism</i>—the benefits and promises of Baptism; the Creed; the Commandments; Prayer; and the Sacraments</i>—especially for those about to be confirmed</li>
+			<li><i>The Prayer Book Order of Holy Communion</i>—a pattern for life in Communion with Christ and his Church</li>
+			<li><i>The Prayer Book Collects, Epistles, and Gospels</i>—the ancient system of presenting the Christian mystery complete and whole each Sunday and holy day</li>
+			<li><i>The Old and New Testaments</i>—how both offer everlasting life to mankind through Christ</li>
+		</ul></li>
+	</ul>
+
+	<img class="banner" src="<?php bloginfo('template_url'); ?>/images/catechesis-project-banner.jpg"/>
+	<ul class="p contents">
+	<h2>Look Within</h2>
+		<br>
+		<li><a href="<?php bloginfo('template_url'); ?>/files/CP_I-am-His-Table-of-Contents-Introduction.docx">Table of Contents, & Introduction</a></li>
+		<li><a href="<?php bloginfo('template_url'); ?>/files/CP_I-am-His-Part-1-A-Place-To-Belong.docx">Part 1 — A Place To Belong</a></li>
+		<li><i>in production</i></li>
+		<li><i>in production</i></li>
+		<li><i>in production</i></li>
+		<li><i>in production</i></li>
+		<li><i>in production</i></li>
+		<li><i>in production</i></li>
+	<time timedate="2015-09-12">Last updated: 12/9/2015</time>
+	</ul>
+
+</section>
+
+<section class="catechesis-project">
+	<h1>Why is It Needed?</h1>
+
+	<p>The Great Commission (Matthew 28:18-20) enjoins the making of disciples of all nations by baptism in the name of the Trinity and by handing on the teaching of Christ – a ministry of spiritual formation in the essentials of doctrine that is called catechesis. Largely because the ancient tradition of catechesis has declined, the faith of many young believers today is shallow and rootless, with little grounding in the Bible or doctrine, and little meaning for their lives. There is widespread agreement that a revival of the tradition of catechesis is necessary to the health and growth of the Church. “Train up”—catechize—“a child in the way he should go: and when he is old, he shall not depart from it” (Proverbs 22:6).</p>
+
+	<h2>This is the need for the <i>Common Prayer Catechesis Project</i>.</h2>
+
+	<p>With curriculum grounded in the Bible and the distinctives of the Anglican tradition of faith and worship, the <i>Common Prayer Catechesis Project</i> will equip churches to catechize their members and newcomers, an important step in fulfilling our calling to “make disciples of all nations.” The curriculum is designed to work with a wide range of churchmanship and liturgical custom, from the 1662 Prayer Book to the 1979 and modern-language adaptations. Its teaching will ground adults, youth, and children in the Catechism, the Bible, Common Prayer and the Sacraments—the essentials of Christian believing, living, and praying.</p>
+
+	<h2>CPCP and Families</h2>
+
+	<p>According to the Bible, Christian parents have a unique gift and vocation to form the faith of their children. In Psalm 78 we read that God:</p>
+
+	<blockquote class="p"><i>“made a covenant with Jacob, and gave Israel  a law, which he commanded our forefathers  to teach their children; / That their posterity might know it, and the children which were yet unborn; / To the intent that when they came up, they might show their children the same; / That they might put their trust in God; and not to forget the works of God, but to keep his commandments.”</i></blockquote>
+
+	<p>Similar teaching is found in Exodus 13:8, Deuteronomy 6:6-9, Proverbs 4:1-13, and Ephesians 6:4. According to the 1662 Prayer Book, one of the causes for which Marriage was ordained by God was “for the procreation of children to be brought up in the fear and nurture of the Lord, and to the praise of his holy Name.” The <i>Common Prayer Catechesis Project</i> will equip Christian parents (with the help of their clergy and other teachers) to catechize their children in the essentials of Christian believing, living, and praying, within the fellowship and worship of the congregation.</p>
+</section>
+
+<section class="catechesis-project">
+	<h1>What Can I Do To Help?</h1>
+
+	<p>The Prayer Book Society has made the Common Prayer Catechesis Project its highest priority. The cost of this project exceeds our resources and donors have already begun to step up, led by a generous gift from the All Souls Anglican Foundation of Oklahoma City. Your financial support will help ensure the timely publication of CPCP. Donations are tax-deductible, and may be made by check or <a href="/contribute/">donation</a>.</p>
+
+	<p>THE COMMON PRAYER CATECHESIS PROJECT</p>
+
+	<p>A ministry of the Prayer Book Society, USA</p>
+
+	<p><a href="<?php bloginfo('template_url'); ?>/files/CPCPFlyer.pdf">Download the flyer to share</a></p>
+
+</section>
+<?php get_footer(); ?>
---
+++ page-print-magazine.php
@@ -0,0 +1,392 @@
+<?php get_header(); ?>
+
+	
+	<div role="img" class="hero-image resources"></div>
+	<div class="page-title"><h1>Anglican Way Magazine</h1></div>
+</header>
+
+
+
+<section class="print-magazine">
+	<h1>2015</h1>
+	<div class="grid-1-2">
+		<h2>Spring 2015</h2>
+	</div>
+	<div class="grid-1-2">
+		<h2>Summer 2015</h2>
+	</div>
+	<div class="grid-1-2">
+		<h2>Fall 2015</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2015-03.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2015-03-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-2">
+		<h2>Winter 2015</h2>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2014</h1>
+	<div class="grid-1-2">
+		<h2>Spring 2014</h2>
+	</div>
+	<div class="grid-1-2">
+		<h2>Summer 2014</h2>
+	</div>
+	<div class="grid-1-2">
+		<h2>Fall 2014</h2>
+	</div>
+	<div class="grid-1-2">
+		<h2>Winter 2014</h2>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2013</h1>
+	<div class="grid-1-2">
+		<h2>Spring 2013</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2013-01.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2013-01-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-2">
+		<h2>Summer 2013</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2013-02.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2013-02-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-2">
+		<h2>Fall 2013</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2013-03.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2013-03-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-2">
+		<h2>Winter 2013</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2013-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2013-04-cover.jpg"/></a>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2012</h1>
+	<div class="grid-1-2">
+		<h2>Spring 2012</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2012-01.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2012-01-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-2">
+		<h2>Summer 2012</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2012-02.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2012-02-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-2">
+		<h2 class="missing">Fall 2012</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2012-03.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2012-03-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-2">
+		<h2 class="missing">Winter 2012</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2012-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2012-04-cover.jpg"/></a>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2011</h1>
+	<div class="grid-1-2">
+		<h2 class="missing">Winter 2011</h2>
+	</div>
+	<div class="grid-1-2">
+		<h2>Spring 2011</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2011-02.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2011-02-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-2">
+		<h2>Summer 2011</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2011-03.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2011-03-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-2">
+		<h2>Fall 2011</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2011-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2011-04-cover.jpg"/></a>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2010</h1>
+	<div class="grid-1-2">
+		<h2>Winter 2010</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2010-01.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2010-01-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-2">
+		<h2>Spring 2010</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2010-02.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2010-02-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-2">
+		<h2>Summer 2010</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2010-03.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2010-03-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-2">
+		<h2>Fall 2010</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2010-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2010-04-cover.png"/></a>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2009</h1>
+	<div class="grid-1-3">
+		<h2>January/February 2009</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2009-01-02.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2009-01-02-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>March/April 2009</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2009-03-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2009-03-04-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>May/June 2009</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2009-05-06.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2009-05-06-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>July/August 2009</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2009-07-08.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2009-07-08-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2 class="missing">September/October 2009</h2>
+	</div>
+	<div class="grid-1-3">
+		<h2 class="missing">November/December 2009</h2>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2008</h1>
+	<div class="grid-1-3">
+		<h2>January/February 2008</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2008-01-02.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2008-01-02-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>March/April 2008</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2008-03-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2008-03-04-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>May/June 2008</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2008-05-06.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2008-05-06-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>July/August 2008</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2008-07-08.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2008-07-08-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>September/October 2008</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2008-09-10.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2008-09-10-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>November/December 2008</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2008-11-12.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2008-11-12-cover.gif"/></a>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2007</h1>
+	<div class="grid-1-3">
+		<h2>January/February 2007</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2007-01-02.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2007-01-02-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>March/April 2007</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2007-03-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2007-03-04-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2 class="missing">May/June 2007</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2007-05-06.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2007-05-06-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>July/August 2007</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2007-07-08.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2007-07-08-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>September/October 2007</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2007-09-10.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2007-09-10-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>November/December 2007</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2007-11-12.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2007-11-12-cover.gif"/></a>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2006</h1>
+	<div class="grid-1-3">
+		<h2>January/February 2006</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2006-01-02.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2006-01-02-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>March/April 2006</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2006-03-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2006-03-04-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>May/June 2006</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2006-05-06.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2006-05-06-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>July/August 2006</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2006-07-08.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2006-07-08-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2 class="missing">September/October 2006</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2006-09-10.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2006-09-10-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>November/December 2006</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2006-11-12.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2006-11-12-cover.gif"/></a>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2005</h1>
+	<div class="grid-1-3">
+		<h2>January/February 2005</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2005-01-02.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2005-01-02-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>March/April 2005</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2005-03-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2005-03-04-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2 class="missing">May/June 2005</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2005-05-06.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2005-05-06-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>July/August 2005</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2005-07-08.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2005-07-08-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>September/October 2005</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2005-09-10.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2005-09-10-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>November/December 2005</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2005-11-12.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2005-11-12-cover.gif"/></a>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2004</h1>
+	<div class="grid-1-3">
+		<h2>January/February 2004</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2004-01-02.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2004-01-02-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>March/April 2004</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2004-03-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2004-03-04-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2 class="missing">May/June 2004</h2>
+	</div>
+	<div class="grid-1-3">
+		<h2>July/August 2004</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2004-07-08.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2004-07-08-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>September/October 2004</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2004-09-10.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2004-09-10-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>November/December 2004</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2004-11-12.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2004-11-12-cover.gif"/></a>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2003</h1>
+	<div class="grid-1-3">
+		<h2>January/February 2003</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2003-01-02.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2003-01-02-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>March/April 2003</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2003-03-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2003-03-04-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2 class="missing">May/June 2003</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2003-05-06.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2003-05-06-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>July/August 2003</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2003-07-08.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2003-07-08-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>September/October 2003</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2003-09-10.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2003-09-10-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2 class="missing">November/December 2003</h2>
+	</div>
+</section>
+
+
+<section class="print-magazine">
+	<h1>2002</h1>
+	<div class="grid-1-3">
+		<h2>January/February 2002</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2002-01-02.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2002-01-02-cover.jpg"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>March/April 2002</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2002-03-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2002-03-04-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2 class="missing">May/June 2002</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2002-05-06.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2002-05-06-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>July/August 2002</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2002-07-08.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2002-07-08-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>September/October 2002</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2002-09-10.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2002-09-10-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>November/December 2002</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2002-11-12.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2002-11-12-cover.gif"/></a>
+	</div>
+</section>
+
+<section class="print-magazine">
+	<h1>2001</h1>
+	<div class="grid-1-3">
+		<h2 class="missing">January/February 2001</h2>
+	</div>
+	<div class="grid-1-3">
+		<h2>March/April 2001</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2001-03-04.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2001-03-04-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2 class="missing">May/June 2001</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2001-05-06.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2001-05-06-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>July/August 2001</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2001-07-08.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2001-07-08-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>September/October 2001</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2001-09-10.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2001-09-10-cover.gif"/></a>
+	</div>
+	<div class="grid-1-3">
+		<h2>November/December 2001</h2>
+		<a href="<?php bloginfo('template_url'); ?>/files/2001-11-12.pdf"><img src="<?php bloginfo('template_url'); ?>/files/2001-11-12-cover.gif"/></a>
+	</div>
+</section>
+
+
+<style>
+.grid-1-2:nth-of-type(1),
+.grid-1-2:nth-of-type(3) {
+	margin-left: 100px;
+}
+</style>
+
+
+
+<?php get_footer(); ?>
